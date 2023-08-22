ff9c6abacd766cef0146746270ea572283c0b0d4
IsmamSoft <ismamsoft@gmail.com>
2023-08-22 13:28:57 +0600

    Update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import App from './components/App.vue';
+import router from './router/router.js';
+
+// createApp(App).mount("#app")
+createApp(App).use(router).mount("#app")
