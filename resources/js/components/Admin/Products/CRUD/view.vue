dd12c785ead9e64948a237dec7ce893d52c9ae09
IsmamSoft <ismamsoft@gmail.com>
2023-08-26 12:31:27 +0600

    Tables

--- resources/js/components/Admin/Products/CRUD/view.vue
+++ resources/js/components/Admin/Products/CRUD/view.vue
@@ -2,10 +2,9 @@
 import Header from "../../Header/Header.vue";
 import Side from "../../Header/Side.vue";
 import Footer from "../../Header/Footer.vue";
-
 </script>
 <template>
-   <div class="container-scroller">
+    <div class="container-scroller">
         <Header />
         <!-- partial -->
         <div class="container-fluid page-body-wrapper">
@@ -29,7 +28,15 @@ import Footer from "../../Header/Footer.vue";
                                     class="breadcrumb-item active"
                                     aria-current="page"
                                 >
-                                    <span></span>Overview
+                                    <span></span
+                                    ><button
+                                        type="button"
+                                        class="btn btn-primary"
+                                        data-bs-toggle="modal"
+                                        data-bs-target="#staticBackdrop"
+                                    >
+                                        Launch static backdrop modal
+                                    </button>
                                     <i
                                         class="mdi mdi-alert-circle-outline icon-sm text-primary align-middle"
                                     ></i>
@@ -38,13 +45,86 @@ import Footer from "../../Header/Footer.vue";
                         </nav>
                     </div>
 
-
-
-
-
+                    <div class="card">
+                        <div class="card-body"></div>
+                    </div>
+                    <br />
+                    <div class="card">
+                        <div class="card-body">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">First</th>
+                                        <th scope="col">Last</th>
+                                        <th scope="col">Handle</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">1</th>
+                                        <td>Mark</td>
+                                        <td>Otto</td>
+                                        <td>@mdo</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">2</th>
+                                        <td>Jacob</td>
+                                        <td>Thornton</td>
+                                        <td>@fat</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">3</th>
+                                        <td colspan="2">Larry the Bird</td>
+                                        <td>@twitter</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
 
                 <Footer />
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal -->
+    <div
+        class="modal fade"
+        id="staticBackdrop"
+        data-bs-backdrop="static"
+        data-bs-keyboard="false"
+        tabindex="-1"
+        aria-labelledby="staticBackdropLabel"
+        aria-hidden="true"
+    >
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="staticBackdropLabel">
+                        Modal title
+                    </h1>
+                    <button
+                        type="button"
+                        class="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                    ></button>
+                </div>
+                <div class="modal-body">...</div>
+                <div class="modal-footer">
+                    <button
+                        type="button"
+                        class="btn btn-secondary"
+                        data-bs-dismiss="modal"
+                    >
+                        Close
+                    </button>
+                    <button type="button" class="btn btn-primary">
+                        Understood
+                    </button>
+                </div>
             </div>
         </div>
     </div>
