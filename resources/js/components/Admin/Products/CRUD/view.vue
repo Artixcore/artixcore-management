06540da07fa637ca53ff433d857da41d9c1bf20d
IsmamSoft <ismamsoft@gmail.com>
2023-09-19 09:27:15 +0600

    ..

--- resources/js/components/Admin/Products/CRUD/view.vue
+++ resources/js/components/Admin/Products/CRUD/view.vue
@@ -41,11 +41,6 @@ import Footer from "../../Header/Footer.vue";
                         </nav>
                     </div>
 
-                    <div class="card">
-                        <div class="card-body">
-                            {{ message }}
-                        </div>
-                    </div>
                     <br />
                     <div class="card">
                         <div class="card-body">
@@ -87,45 +82,7 @@ import Footer from "../../Header/Footer.vue";
         </div>
     </div>
 
-    <!-- Modal -->
-    <div
-        class="modal fade"
-        id="staticBackdrop"
-        data-bs-backdrop="static"
-        data-bs-keyboard="false"
-        tabindex="-1"
-        aria-labelledby="staticBackdropLabel"
-        aria-hidden="true"
-    >
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="staticBackdropLabel">
-                        Modal title
-                    </h1>
-                    <button
-                        type="button"
-                        class="btn-close"
-                        data-bs-dismiss="modal"
-                        aria-label="Close"
-                    ></button>
-                </div>
-                <div class="modal-body">...</div>
-                <div class="modal-footer">
-                    <button
-                        type="button"
-                        class="btn btn-secondary"
-                        data-bs-dismiss="modal"
-                    >
-                        Close
-                    </button>
-                    <button type="button" class="btn btn-primary">
-                        Understood
-                    </button>
-                </div>
-            </div>
-        </div>
-    </div>
+
 </template>
 <script>
 import axios from "axios";
