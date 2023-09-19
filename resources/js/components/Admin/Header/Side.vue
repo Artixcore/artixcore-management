c6be3c8f70cf53ac630a63a39b347f9083e7c8a4
IsmamSoft <ismamsoft@gmail.com>
2023-09-19 09:31:34 +0600

    ..

--- resources/js/components/Admin/Header/Side.vue
+++ resources/js/components/Admin/Header/Side.vue
@@ -1,19 +1,6 @@
 <template>
      <nav class="sidebar sidebar-offcanvas" id="sidebar">
           <ul class="nav">
-            <!-- <li class="nav-item nav-profile">
-              <a href="#" class="nav-link">
-                <div class="nav-profile-image">
-                  <span class="login-status online"></span>
-
-                </div>
-                <div class="nav-profile-text d-flex flex-column">
-                  <span class="font-weight-bold mb-2">David Grey. H</span>
-                  <span class="text-secondary text-small">Project Manager</span>
-                </div>
-                <i class="mdi mdi-bookmark-check text-success nav-profile-badge"></i>
-              </a>
-            </li> -->
             <li class="nav-item">
               <router-link class="nav-link" to="/Dashboard">
                 <span class="menu-title">Dashboard</span>
